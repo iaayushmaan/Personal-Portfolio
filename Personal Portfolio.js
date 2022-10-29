ef8fe346d5cc4220983a1710df1c32e11380e3cd
iaayushmaan <aayush10tawer@gmail.com>
2022-10-30 02:53:38 +0530

    Personal-Portfolio

---
+++ Personal Portfolio.js
@@ -0,0 +1,26 @@
+var navbar = document.getElementById('navbar');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100 ) {
+        navbar.classList.add("nav-colored");
+        navbar.classList.remove("navbar");
+    } 
+    else {
+        navbar.classList.add("navbar");
+        navbar.classList.remove("nav-colored");
+    }
+};
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+      x.className += " responsive";
+    } else {
+      x.className = "menu";
+    }
+    var y = document.getElementById("navbar");
+    if (y.className === "navbar") {
+        navbar.classList.add("nav-colored");
+    } else {
+        navbar.classList.add("navbar");
+    }
+  }
